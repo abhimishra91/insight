0c283f5fd199ccb64cd0e4363c18a49b5de658a1
Abhishek Kumar Mishra <abhimishra.91@gmail.com>
2020-07-08 18:22:11 +0800

    IN-5 Updates

--- src_fastapi/sentimentpro.py
+++ src_fastapi/sentimentpro.py
@@ -9,12 +9,19 @@ device = torch.device("cpu")
 
 class SentimentProcessor:
     def __init__(self, model: str = None, service: str = "sentiment"):
+        """
+        Constructor to the class that does the Sentiment Analysis Processing in the back end
+        :param model: Transfomer model that will be used for sentiment analysis
+        :param service: string to represent the service, this will be defaulted to sentiment
+        """
         if model is None:
             model = "distilbert"
-
+        # path to all the files that will be used for inference
         self.path = f"./{service}/{model}/"
+        # json file for mapping of network output to the correct category
         self.mapping = self.path + "mapping.json"
         self.model_path = self.path + "model.bin"
+        # Selecting the correct model based on the passed madel input. Default distilbert
         if model == "roberta":
             self.model = RobertaClass()
             self.tokenizer = RobertaTokenizerFast.from_pretrained(self.path)
@@ -32,6 +39,12 @@ class SentimentProcessor:
             self.config = json.load(f)
 
     def tokenize(self, input_text: str, query: str = None):
+        """
+        Method to tokenize the textual input
+        :param input_text: Input text
+        :param query: Query in case of Question Answering service.
+        :return: Returns encoded text for inference
+        """
         inputs = self.tokenizer.encode_plus(
             input_text,
             query,
@@ -46,9 +59,19 @@ class SentimentProcessor:
         return inputs
 
     def lookup(self):
+        """
+        Function to perform look up against the mapping json file. Only applicable for classificaiton and sentiment analysis.
+        :return: Correct category for the prediction.
+        """
         return self.config[str(int(self.pred.item()))]
 
     def inference(self, input_text: str, query: str = None):
+        """
+        Method to perform the inference
+        :param input_text: Input text for the inference
+        :param query: Input qwuery in case of QnA
+        :return: correct category and confidence for that category
+        """
         self.tokenized_inputs = self.tokenize(input_text, query)
         self.input_ids = self.tokenized_inputs["input_ids"]
         self.attention_mask = self.tokenized_inputs["attention_mask"]
