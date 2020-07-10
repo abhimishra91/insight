5f29340856cc6edb5663d6f755ee261c75413b3b
Abhishek Kumar Mishra <abhimishra.91@gmail.com>
2020-07-10 18:43:07 +0800

    IN-8 updates

--- src_fastapi/sentiment/distilbert/network.py
+++ src_fastapi/sentiment/distilbert/network.py
@@ -5,13 +5,13 @@ from transformers import DistilBertModel
 class DistilBertClass(torch.nn.Module):
     def __init__(self):
         super(DistilBertClass, self).__init__()
-        self.model_layer = DistilBertModel.from_pretrained('distilbert-base-uncased')
-        self.pre_classifier = torch.nn.Linear(768,768)
+        self.model_layer = DistilBertModel.from_pretrained("distilbert-base-uncased")
+        self.pre_classifier = torch.nn.Linear(768, 768)
         self.dropout = torch.nn.Dropout(0.3)
         self.classifier = torch.nn.Linear(768, 2)
-    
+
     def forward(self, input_ids, attention_mask):
-        output_1= self.model_layer(input_ids = input_ids, attention_mask = attention_mask)
+        output_1 = self.model_layer(input_ids=input_ids, attention_mask=attention_mask)
         hidden_state = output_1[0]
         pooler = hidden_state[:, 0]
         pooler = self.pre_classifier(pooler)
