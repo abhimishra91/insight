6a78be100c7d9defd2679ec9f468143f3bfec384
Abhishek Kumar Mishra <abhimishra.91@gmail.com>
2020-07-18 04:47:28 +0800

    updates for IN-13

--- src_streamlit/NLPfiy.py
+++ src_streamlit/NLPfiy.py
@@ -41,72 +41,88 @@ class MakeCalls:
         return json.loads(result.text)
 
 
-def disaply_page(service: str, models_dict: dict):
-    """
-    This function is used to generate the page for each service.
-    :param service: String of the service being selected from the side bar.
-    :param models_dict: Dictionary of Model and its information. This is used to render elements of the page.
-    :return: model, input_text run_button: Selected model from the drop down, input text by the user and run botton to kick off the process.
-    """
-    st.header(service)
-    model_name = list()
-    model_info = list()
-    for i in models_dict.keys():
-        model_name.append(models_dict[i]["name"])
-        model_info.append(models_dict[i]["info"])
-    st.sidebar.header("Model Information")
-    for i in range(len(model_name)):
-        st.sidebar.subheader(model_name[i])
-        st.sidebar.info(model_info[i])
-    model: str = st.selectbox("Select the Trained Model", model_name)
-    input_text: str = st.text_area("Enter Text here")
-    if service == "Information Extraction":
-        query: str = st.text_input("Enter query here.")
-        run_button: bool = st.button("Run")
-        return model, input_text, query, run_button
-    else:
-        run_button: bool = st.button("Run")
-        return model, input_text, run_button
+class Display:
+    def __init__(self):
+        st.title("Insight")
+        st.sidebar.header("Select the NLP Service")
+        self.service_options = st.sidebar.selectbox(
+            label="",
+            options=[
+                "Project Insight",
+                "News Classification",
+                "Named Entity Recognition",
+                "Sentiment Analysis",
+                "Summarization",
+                "Information Extraction",
+            ],
+        )
+        self.service = {
+            "Project Insight": "about",
+            "News Classification": "classification",
+            "Named Entity Recognition": "ner",
+            "Sentiment Analysis": "sentiment",
+            "Summarization": "summ",
+            "Information Extraction": "qna",
+        }
+        
+
+    def static_elements(self):
+        return self.service[self.service_options]
+        
+
+    def dynamic_element(self, service: str, models_dict: dict):
+        """
+        This function is used to generate the page for each service.
+        :param service: String of the service being selected from the side bar.
+        :param models_dict: Dictionary of Model and its information. This is used to render elements of the page.
+        :return: model, input_text run_button: Selected model from the drop down, input text by the user and run botton to kick off the process.
+        """
+        st.header(self.service_options)
+        model_name = list()
+        model_info = list()
+        for i in models_dict.keys():
+            model_name.append(models_dict[i]["name"])
+            model_info.append(models_dict[i]["info"])
+        st.sidebar.header("Model Information")
+        for i in range(len(model_name)):
+            st.sidebar.subheader(model_name[i])
+            st.sidebar.markdown(model_info[i])
+        model: str = st.selectbox("Select the Trained Model", model_name)
+        input_text: str = st.text_area("Enter Text here")
+        if service == "qna":
+            query: str = st.text_input("Enter query here.")
+            run_button: bool = st.button("Run")
+            return model, input_text, query, run_button
+        else:
+            run_button: bool = st.button("Run")
+            return model, input_text, run_button   
+
 
 
 def main():
-    st.title("Insight")
-    st.sidebar.header("Select the NLP Service")
-    service_options = st.sidebar.selectbox(
-        label="",
-        options=[
-            "Project Insight",
-            "News Classification",
-            "Named Entity Recognition",
-            "Sentiment Analysis",
-            "Summarization",
-            "Information Extraction",
-        ],
-    )
-    service = {
-        "Project Insight": "about",
-        "News Classification": "classification",
-        "Named Entity Recognition": "ner",
-        "Sentiment Analysis": "sentiment",
-        "Summarization": "summ",
-        "Information Extraction": "qna",
-    }
+
+    page = Display()
+    service = page.static_elements()
     apicall = MakeCalls()
-    if service[service_options] == "about":
-        st.header("This is the Project Insight about Page...")
+    if service == "about":
+        st.header("NLP as a Service")
+        st.write("The users can leverage fine-tuned language models to perform multiple downstream tasks, via GUI and API access.")
+        st.write("Insight backed in designed in a way that developers can also add-in their own fine-tuned models on different datasets and use them for prediction.")
+        st.write("To use this solution, select a service from the dropdown in the side bar. Details of pre-loaded  pre-trained model will be available based on the service.")
+        st.write("Fill in the text on which you want to run the service and then let the magic happen.")
     else:
-        model_details = apicall.model_list(service=service[service_options])
-        if service_options == "Information Extraction":
-            model, input_text, query, run_button = disaply_page(
-                service_options, model_details
+        model_details = apicall.model_list(service=service)
+        if service == "qna":
+            model, input_text, query, run_button = page.dynamic_element(
+                service, model_details
             )
         else:
-            model, input_text, run_button = disaply_page(service_options, model_details)
+            model, input_text, run_button = page.dynamic_element(service, model_details)
             query = str()
 
         if run_button:
             result = apicall.run_inference(
-                service=service[service_options],
+                service=service,
                 model=model.lower(),
                 text=input_text,
                 query=query.lower(),
